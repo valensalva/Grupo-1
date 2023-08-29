b633ab652c5300a9272318d20dd9af1c412aede2
frncabre <127581455+frncabre@users.noreply.github.com>
2023-08-29 14:36:09 -0300

    Create guardar_pass.php

---
+++ Codigo/Recover/guardar_pass.php
@@ -0,0 +1,18 @@
+<?php
+	require 'funcs/conexion.php';
+	require 'funcs/funcs.php';
+
+	$user_id = $mysqli->real_escape_string($_POST['user_id']);
+	$token = $mysqli->real_escape_string($_POST['token']);
+	$password = $mysqli->real_escape_string($_POST['password']);
+	$con_password = $mysqli->real_escape_string($_POST['con_password']);
+
+	if(validaPassword($password,$user_id, $token)){
+		echo "password modificada";
+		echo "<br> <a href='index.php'> Iniciar Sesion </a>";
+	}else{
+		echo "Error al modificar la contraseña";
+	}else{
+		echo "Las contraseña no coinciden";
+	}
+?>
