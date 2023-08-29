17de2e370102cedb1608ce05be063c704537d782
frncabre <127581455+frncabre@users.noreply.github.com>
2023-08-29 14:35:31 -0300

    Create cambio_pass.php

---
+++ Codigo/Recover/cambio_pass.php
@@ -0,0 +1,19 @@
+<?php
+	include('connect.php');
+	include('functions.php');
+
+	if(empty($_GET['user_id'])){
+		header('Location: index.php');
+	}
+	if (empty($_GET['token'])){
+		header('Location: index.php');
+	}
+
+	$user_id = $mysqli->real_escape_string($_GET['user_id']);
+	$token = $mysqli->real_escape_string($_GET['token']);
+
+	if(!verificaTokenPass($user_id, $token)){
+		echo 'No se pudo verificar los Datos';
+		exit;
+	}
+?>
